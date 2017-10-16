e29a89613fe0653a9dee63a0205aa830a37a35db
Andrey Shlykov <andrew.shlykov@ya.ru>
2017-10-16 04:33:19 +0300

    cmake: add dependencies from glew and glfw

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,54 @@
 // Created by Андрей on 15.10.17.
 //
 
+#include <GL/glew.h>
+
+#define GLFW_INCLUDE_GLCOREARB
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 
 int main()
 {
-    std::cout << "hello" << std::endl;
+    if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(640, 640, "test", nullptr, nullptr);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    glewExperimental = GL_TRUE;
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+    {
+        printf("Error: %s\n", glewGetErrorString(err));
+        return -1;
+    }
+
+    glGetError();
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+    }
+
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
